8a882bf2d0e153261d7291a9f46a60d3ad0a013c
Brian Austin <brianjaustin@gmail.com>
2019-08-09 22:13:54 -0400

    Add parse_netsh with test and fixture
    Fixture (netsh01_windows81.txt) is a modified version of the one provided in booyaa/wifiscanner#5.

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,8 @@
 //! Alternatively if you've cloned the the Git repo, you can run the above example
 //! using: `cargo run --example scan`.
 
+#[macro_use]
+extern crate itertools;
 extern crate regex;
 
 #[doc(no_inline)]
@@ -103,6 +105,11 @@ pub fn scan() -> Result<Vec<Wifi>, Error> {
     parse_iwlist(&data)
 }
 
+/// Returns a list of WiFi hotspots in your area - (Windows) uses `netsh`
+#[cfg(target_os = "windows")]
+pub fn scan() -> Result<Vec<Wifi>, Error> {
+    Err(Error::CommandNotFound)
+}
 
 #[allow(dead_code)]
 fn parse_airport(network_list: &str) -> Result<Vec<Wifi>, Error> {
@@ -219,6 +226,55 @@ fn parse_iwlist(network_list: &str) -> Result<Vec<Wifi>, Error> {
             }
         } // for
     }
+    Ok(wifis)
+}
+
+#[allow(dead_code)]
+fn parse_netsh(network_list: &str) -> Result<Vec<Wifi>, Error> {
+    let mut wifis = Vec::new();
+
+    // Regex for matching split, SSID and MAC, since these aren't pulled directly
+    let split_regex = Regex::new("\nSSID").map_err(|_| Error::SyntaxRegexError)?;
+    let ssid_regex = Regex::new("^ [0-9]* : ").map_err(|_| Error::SyntaxRegexError)?;
+    let mac_regex = Regex::new("[a-fA-F0-9:]{17}").map_err(|_| Error::SyntaxRegexError)?;
+
+    for block in split_regex.split(network_list) {
+        let mut wifi_macs = Vec::new();
+        let mut wifi_ssid = String::new();
+        let mut wifi_channels = Vec::new();
+        let mut wifi_rssi = Vec::new();
+        let mut wifi_security = String::new();
+
+        for line in block.lines() {
+            if ssid_regex.is_match(line) {
+                wifi_ssid = line.split(":").nth(1).unwrap_or("").trim().to_string();
+            } else if line.find("Authentication").is_some() {
+                wifi_security = line.split(":").nth(1).unwrap_or("").trim().to_string();
+            } else if line.find("BSSID").is_some() {
+                let captures = mac_regex.captures(line).unwrap();
+                wifi_macs.push(captures.at(0).unwrap());
+            } else if line.find("Signal").is_some() {
+                let percent = line.split(":").nth(1).unwrap_or("").trim().replace("%", "");
+                let percent: i32 = percent.parse().unwrap();
+                wifi_rssi.push(percent / 2 - 100);
+            } else if line.find("Channel").is_some() {
+                wifi_channels.push(line.split(":").nth(1).unwrap_or("").trim().to_string());
+            }
+        }
+
+        for (mac, channel, rssi) in izip!(wifi_macs, wifi_channels, wifi_rssi) {
+            wifis.push(
+                Wifi {
+                    mac: mac.to_string(),
+                    ssid: wifi_ssid.to_string(),
+                    channel: channel.to_string(),
+                    signal_level: rssi.to_string(),
+                    security: wifi_security.to_string(),
+                }
+            );
+        }
+    }
+
     Ok(wifis)
 }
 
@@ -351,4 +407,50 @@ fn should_parse_airport() {
     let last = result.len() - 1;
     assert_eq!(expected[0], result[0]);
     assert_eq!(expected[1], result[last]);
+}
+
+#[test]
+fn should_parse_netsh() {
+    use std::fs;
+
+    // Note: formula for % to dBm is (% / 100) - 100
+    let expected = vec![
+        Wifi {
+            mac: "ab:cd:ef:01:23:45".to_string(),
+            ssid: "Vodafone Hotspot".to_string(),
+            channel: "6".to_string(),
+            signal_level: "-92".to_string(),
+            security: "Open".to_string(),
+        },
+        Wifi {
+            mac: "ab:cd:ef:01:23:45".to_string(),
+            ssid: "Vodafone Hotspot".to_string(),
+            channel: "6".to_string(),
+            signal_level: "-73".to_string(),
+            security: "Open".to_string(),
+        },
+        Wifi {
+            mac: "ab:cd:ef:01:23:45".to_string(),
+            ssid: "EdaBox".to_string(),
+            channel: "11".to_string(),
+            signal_level: "-82".to_string(),
+            security: "WPA2-Personal".to_string(),
+        },
+        Wifi {
+            mac: "ab:cd:ef:01:23:45".to_string(),
+            ssid: "FRITZ!Box 2345 Cable".to_string(),
+            channel: "1".to_string(),
+            signal_level: "-50".to_string(),
+            security: "WPA2-Personal".to_string(),
+        }
+    ];
+
+    // Load test fixtures
+    let fixture = fs::read_to_string("tests/fixtures/netsh/netsh01_windows81.txt").unwrap();
+
+    let result = parse_netsh(&fixture).unwrap();
+    assert_eq!(expected[0], result[0]);
+    assert_eq!(expected[1], result[1]);
+    assert_eq!(expected[2], result[2]);
+    assert_eq!(expected[3], result[3]);
 }
